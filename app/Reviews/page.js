a07d0c71ea597376454f4bc5074e46ef6ef5c6a8
Syed Muhammad Salman Shah <152138774+danger137@users.noreply.github.com>
2025-03-19 17:30:52 +0500

    NAME

--- app/Reviews/page.js
+++ app/Reviews/page.js
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
+
 export default function Reviews (){
 
     const testimonials = [
--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import Head from "next/head";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import ChatModal from "./components/ChatModal";
+import ChatWidget from "./components/ChatWidget";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -88,6 +90,9 @@ export default function RootLayout({ children }) {
         <Header />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         {children}
+        {/* <ChatModal key={4} /> */}
+        <ChatWidget key={666} />
+
         <Footer></Footer>
       </body>
     </html>
